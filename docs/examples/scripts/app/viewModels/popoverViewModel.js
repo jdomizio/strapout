91bf2cffb46699644124d261719a94c0310910f5
jdomizio <akuenx@gmail.com>
2014-12-08 14:47:25 -0500

    Added popover demos Reordered tooltip demos

--- docs/examples/scripts/app/viewModels/popoverViewModel.js
+++ docs/examples/scripts/app/viewModels/popoverViewModel.js
@@ -8,6 +8,25 @@ define(['knockout', 'jquery', '../../../../../src/popover', 'bootstrap'], functi
 
         /** model for 'simple binding' example */
         this.simple = new Popover();
+
+        this.codeConfigured = new Popover({
+            title: 'Code-Configured',
+            content: 'This message was configured within the viewModel.',
+            options: {
+                trigger: 'hover'
+            }
+        });
+
+        this.inlineConfigured = new Popover();
+
+        this.observabilityDemoPopover = new Popover({
+            title: 'I am a title',
+            content: 'I am a content.',
+            options: {
+                placement: 'bottom',
+                trigger: 'hover'
+            }
+        });
     }
 
     return {
